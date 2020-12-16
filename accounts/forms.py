d046e84a704e2b7f35433e2c29be9d327e57400d
TomekM <tomasz_matuszewski@o2.pl>
2020-12-16 11:51:49 +0100

    profile view, messages

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.forms import (
     UserCreationForm
 )
+from django.contrib.auth.models import User
+
 from accounts.models import Profile
-from django.forms import ImageField
+from django.forms import ImageField, ModelForm
 from django.db.transaction import atomic
 
 
@@ -21,3 +23,17 @@ class SingUpForm(UserCreationForm):
         if commit:
             profile.save()
         return result
+
+
+class UserUpdateForm(ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+
+class ProfileUpdateForm(ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['image']
--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,59 @@
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-
-from accounts.forms import SingUpForm
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+from accounts.forms import SingUpForm, UserUpdateForm, ProfileUpdateForm
 
 
 class SignUpView(CreateView):
-
-    template_name = 'accounts/form.html'
+    template_name = 'accounts/form_signup.html'
     form_class = SingUpForm
     success_url = reverse_lazy('bookstore')
 
 
 class MyLoginView(LoginView):
-    template_name = 'accounts/form.html'
+    template_name = 'accounts/form_login.html'
 
 
-class PasswordChangeViewOwn(PasswordChangeView):
+class PasswordChangeViewOwn(LoginRequiredMixin, PasswordChangeView):
 
-    template_name = 'accounts/form.html'
+    template_name = 'accounts/form_password_change.html'
     success_url = reverse_lazy("bookstore")
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, "Your password has been changed.")
+        return super().form_valid(form)
+
+
+class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+
+    model = User
+    template_name = "accounts/user_profile.html"
+    form_class = UserUpdateForm
+
+    def post(self, request, *args, **kwargs):
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save() and profile_form.save()
+            messages.success(request, f"Your account has been updated!")
+            return redirect('bookstore')
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+        context = {"user_form": user_form, "profile_form": profile_form}
+        return render(request, self.template_name, self.get_context_data(**context))
+
+    def test_func(self):
+        user = self.get_object()
+        if self.request.user == user:
+            return True
+        return False
