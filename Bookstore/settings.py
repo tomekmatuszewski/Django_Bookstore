3fe4a7100f297cef51c03a52848f1094dd3a71e0
TomekM <tomasz_matuszewski@o2.pl>
2020-12-27 21:06:55 +0100

    cleaning, refactoring

--- Bookstore/settings.py
+++ Bookstore/settings.py
@@ -57,11 +57,11 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
--- books/migrations/0005_auto_20201222_1418.py
+++ books/migrations/0005_auto_20201222_1418.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('books', '0004_auto_20201217_1050'),
+        ("books", "0004_auto_20201217_1050"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='book',
-            name='rating',
+            model_name="book",
+            name="rating",
             field=models.DecimalField(decimal_places=1, max_digits=3),
         ),
     ]
